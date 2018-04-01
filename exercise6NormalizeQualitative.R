5b3942e75d0872154d3a57ea74c85a508090771c
Fernando Barbeiro <fernandoabcampos@gmail.com>
2018-04-01 22:04:21 +0200

    Complementary transformation

--- exercise6NormalizeQualitative.R
+++ exercise6NormalizeQualitative.R
@@ -1,9 +1,15 @@
+
+
+
 region_table <- table(gsub("AFRCA", "AFRICA", trimws(gsub("\\s+", " ", df$Region))))
 region_table
 
 table(df$Country)
-df$Region <- gsub("AFRCA", "AFRICA", trimws(gsub("\\s+", " ", df$Region)))
-country_table <- table(df$Country)
+df$Country <- gsub(" And ", " and ", str_to_title(df$Country))
+df$Region <- gsub(" And ", " and ", str_to_title(gsub("AFRCA", "AFRICA", trimws(gsub("\\s+", " ", str_to_upper(df$Region))))))
+table(df$Region)
+
+country_table <- table(str_to_title(df$Country))
 country_table
 
 table(df$Region)
