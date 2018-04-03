2cc4bbcf218157b701364f4cb0901497e214b82b
Fernando Barbeiro <fernandoabcampos@gmail.com>
2018-04-03 18:57:47 +0200

    using a function to print each table

--- exercise8boxplotQuantitative.R
+++ exercise8boxplotQuantitative.R
@@ -1,17 +1,10 @@
 # boxplot for each quatitative variable
 str(df)
 
-# $ HS        : num  7.53 7.51 7.5 7.5 7.41 ...
-# $ LCI       : num  7.46 7.43 7.33 7.42 7.35 ...
-# $ UCI       : num  7.59 7.59 7.67 7.58 7.47 ...
-# $ GPC       : num  1.44 1.53 1.43 1.58 1.41 ...
-# $ Family    : num  1.16 1.15 NA 1.13 1.13 ...
-# $ LE        : num  0.795 0.863 0.867 0.796 0.811 ...
-# $ Freedom   : num  0.579 0.586 0.566 0.596 0.571 ...
-# $ GC        : num  0.445 0.412 0.15 0.358 0.41 ...
-# $ Generosity: num  0.362 0.281 0.477 0.379 0.255 ...
-# $ DR        : num  2.74 2.69 2.83 2.66 2.83 ...
 library(psych)
+library(kableExtra)
+library(knitr)
+
 boxplot(df$HS, main="Happiness Score")
 boxplot(df$LCI, main="Lower Confidence Interv.")
 boxplot(df$UCI, main="Upper Confidence Interv.")
@@ -24,89 +17,132 @@ boxplot(df$Generosity, main="Generosity")
 boxplot(df$DR, main="Dystopia Residual")
 
 
-boxplot.stats(df$GC)
+imprime_grafico <- function(my_data, caption_print) {
+  colnames(my_data)[which(names(my_data) == "media_aritmetica")] <- "Media aritmética"
+  colnames(my_data)[which(names(my_data) == "mediana")] <- "Mediana"
+  colnames(my_data)[which(names(my_data) == "recortada")] <- "Media Recortada"
+  colnames(my_data)[which(names(my_data) == "winsorizada")] <- "Media Winsorizada"
+  colnames(my_data)[which(names(my_data) == "desviacion")] <- "Desviación estándar"
+  kable(
+    my_data
+    , "html", caption = caption_print) %>%
+    kable_styling(bootstrap_options = c("striped", "hover", "bordered")) %>%
+    column_spec(1, background = "#ffd6d6") %>%
+    column_spec(2:4, background = "#e5ffd6") %>%
+    column_spec(5, background = "#ffd6d6") %>%
+    column_spec(6:7, background = "#e5ffd6") %>%
+    add_header_above(c("No Robusto" = 1, "Robusto" = 3, "No Robusto" = 1, "Robusto" = 2)) %>%
+    add_header_above(c("Tendencia Central" = 4, "Dispersión" = 3)) 
+}
 
 
-mean(df$HS)
-median(df$HS)
-mean(df$HS, trim = 0.05)
-winsor.mean(df$HS, trim = 0.05)
-sd(df$HS)
-IQR(df$HS)
-mad(df$HS)
+media_aritmetica <- mean(df$HS)
+mediana <- median(df$HS)
+recortada <- mean(df$HS, trim = 0.05)
+winsorizada <- winsor.mean(df$HS, trim = 0.05)
+desviacion <- sd(df$HS)
+RIC <- IQR(df$HS)
+DAM <-mad(df$HS)
+ex8 <- data.frame(media_aritmetica, mediana, recortada, winsorizada, desviacion, RIC, DAM)
+imprime_grafico(ex8, "Happiness Score")
 
-mean(df$LCI)
-median(df$LCI)
-mean(df$LCI, trim = 0.05)
-winsor.mean(df$LCI, trim = 0.05)
-sd(df$LCI)
-IQR(df$LCI)
-mad(df$LCI)
+media_aritmetica <- mean(df$LCI)
+mediana <- median(df$LCI)
+recortada <- mean(df$LCI, trim = 0.05)
+winsorizada <- winsor.mean(df$LCI, trim = 0.05)
+desviacion <- sd(df$LCI)
+RIC <- IQR(df$LCI)
+DAM <-mad(df$LCI)
+ex8 <- data.frame(media_aritmetica, mediana, recortada, winsorizada, desviacion, RIC, DAM)
+imprime_grafico(ex8, "Lower Confidence Interval")
 
-mean(df$UCI)
-median(df$UCI)
-mean(df$UCI, trim = 0.05)
-winsor.mean(df$UCI, trim = 0.05)
-sd(df$UCI)
-IQR(df$UCI)
-mad(df$UCI)
+media_aritmetica <- mean(df$UCI)
+mediana <- median(df$UCI)
+recortada <- mean(df$UCI, trim = 0.05)
+winsorizada <- winsor.mean(df$UCI, trim = 0.05)
+desviacion <- sd(df$UCI)
+RIC <- IQR(df$UCI)
+DAM <-mad(df$UCI)
+ex8 <- data.frame(media_aritmetica, mediana, recortada, winsorizada, desviacion, RIC, DAM)
+imprime_grafico(ex8, "Upper Confidence Interval")
 
-mean(df$GPC)
-median(df$GPC)
-mean(df$GPC, trim = 0.05)
-winsor.mean(df$GPC, trim = 0.05)
-sd(df$GPC)
-IQR(df$GPC)
-mad(df$GPC)
+##########
+media_aritmetica <- mean(df$GPC)
+mediana <- median(df$GPC)
+recortada <- mean(df$GPC, trim = 0.05)
+winsorizada <- winsor.mean(df$GPC, trim = 0.05)
+desviacion <- sd(df$GPC)
+RIC <- IQR(df$GPC)
+DAM <-mad(df$GPC)
+ex8 <- data.frame(media_aritmetica, mediana, recortada, winsorizada, desviacion, RIC, DAM)
+imprime_grafico(ex8, "GDP per Capita")
 
-mean(df$Family, na.rm = TRUE)
-median(df$Family, na.rm = TRUE)
-mean(df$Family, trim = 0.05, na.rm = TRUE)
-winsor.mean(df$Family, trim = 0.05)
-sd(df$Family, na.rm = TRUE)
-IQR(df$Family, na.rm = TRUE)
-mad(df$Family, na.rm = TRUE)
+##########
+media_aritmetica <- mean(df$Family, na.rm = TRUE)
+mediana <- median(df$Family, na.rm = TRUE)
+recortada <- mean(df$Family, trim = 0.05, na.rm = TRUE)
+winsorizada <- winsor.mean(df$Family, trim = 0.05)
+desviacion <- sd(df$Family, na.rm = TRUE)
+RIC <- IQR(df$Family, na.rm = TRUE)
+DAM <-mad(df$Family, na.rm = TRUE)
+ex8 <- data.frame(media_aritmetica, mediana, recortada, winsorizada, desviacion, RIC, DAM)
+imprime_grafico(ex8, "Family")
 
-mean(df$LE)
-median(df$LE)
-mean(df$LE, trim = 0.05)
-winsor.mean(df$LE, trim = 0.05)
-sd(df$LE)
-IQR(df$LE)
-mad(df$LE)
+##########
+media_aritmetica <- mean(df$LE)
+mediana <- median(df$LE)
+recortada <- mean(df$LE, trim = 0.05)
+winsorizada <- winsor.mean(df$LE, trim = 0.05)
+desviacion <- sd(df$LE)
+RIC <- IQR(df$LE)
+DAM <-mad(df$LE)
+ex8 <- data.frame(media_aritmetica, mediana, recortada, winsorizada, desviacion, RIC, DAM)
+imprime_grafico(ex8, "Life Expectancy")
+
+##########
+media_aritmetica <- mean(df$Freedom)
+mediana <- median(df$Freedom)
+recortada <- mean(df$Freedom, trim = 0.05)
+winsorizada <- winsor.mean(df$Freedom, trim = 0.05)
+desviacion <- sd(df$Freedom)
+RIC <- IQR(df$Freedom)
+DAM <-mad(df$Freedom)
+ex8 <- data.frame(media_aritmetica, mediana, recortada, winsorizada, desviacion, RIC, DAM)
+imprime_grafico(ex8, "Freedom")
 
 
-mean(df$Freedom)
-median(df$Freedom)
-mean(df$Freedom, trim = 0.05)
-winsor.mean(df$Freedom, trim = 0.05)
-sd(df$Freedom)
-IQR(df$Freedom)
-mad(df$Freedom)
+##########
+media_aritmetica <- mean(df$Generosity)
+mediana <- median(df$Generosity)
+recortada <- mean(df$Generosity, trim = 0.05)
+winsorizada <- winsor.mean(df$Generosity, trim = 0.05)
+desviacion <- sd(df$Generosity)
+RIC <- IQR(df$Generosity)
+DAM <-mad(df$Generosity)
+ex8 <- data.frame(media_aritmetica, mediana, recortada, winsorizada, desviacion, RIC, DAM)
+imprime_grafico(ex8, "Generosity")
 
-mean(df$Generosity)
-median(df$Generosity)
-mean(df$Generosity, trim = 0.05)
-winsor.mean(df$Generosity, trim = 0.05)
-sd(df$Generosity)
-IQR(df$Generosity)
-mad(df$Generosity)
 
-mean(df$GC)
-median(df$GC)
-mean(df$GC, trim = 0.05)
-winsor.mean(df$GC, trim = 0.05)
-sd(df$GC)
-IQR(df$GC)
-mad(df$GC)
+##########
+media_aritmetica <- mean(df$GC)
+mediana <- median(df$GC)
+recortada <- mean(df$GC, trim = 0.05)
+winsorizada <- winsor.mean(df$GC, trim = 0.05)
+desviacion <- sd(df$GC)
+RIC <- IQR(df$GC)
+DAM <-mad(df$GC)
+ex8 <- data.frame(media_aritmetica, mediana, recortada, winsorizada, desviacion, RIC, DAM)
+imprime_grafico(ex8, "Government Corruption")
 
-mean(df$DR)
-median(df$DR)
-mean(df$DR, trim = 0.05)
-winsor.mean(df$DR, trim = 0.05)
-sd(df$DR)
-IQR(df$DR)
-mad(df$DR)
-
+##########
+media_aritmetica <- mean(df$DR)
+mediana <- median(df$DR)
+recortada <- mean(df$DR, trim = 0.05)
+winsorizada <- winsor.mean(df$DR, trim = 0.05)
+desviacion <- sd(df$DR)
+RIC <- IQR(df$DR)
+DAM <-mad(df$DR)
+ex8 <- data.frame(media_aritmetica, mediana, recortada, winsorizada, desviacion, RIC, DAM)
+imprime_grafico(ex8, "Dystopia Residual")
 
 
