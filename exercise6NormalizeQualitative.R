43cfaca93474be58a8af33ac673277d1b1c9fa6c
Fernando Barbeiro <fernandoabcampos@gmail.com>
2018-04-03 18:56:38 +0200

    Commenting unnecessary code

--- exercise6NormalizeQualitative.R
+++ exercise6NormalizeQualitative.R
@@ -1,15 +1,18 @@
 
 library(stringr)
 
-region_table <- table(gsub("AFRCA", "AFRICA", trimws(gsub("\\s+", " ", df$Region))))
-region_table
-
-table(df$Country)
+#region_table <- table(gsub("AFRCA", "AFRICA", trimws(gsub("\\s+", " ", df$Region))))
+#region_table
+#?str_to_upper()
+#table(df$Country)
 df$Country <- gsub(" And ", " and ", str_to_title(df$Country))
 df$Region <- gsub(" And ", " and ", str_to_title(gsub("AFRCA", "AFRICA", trimws(gsub("\\s+", " ", str_to_upper(df$Region))))))
 table(df$Region)
+#table(df$Country)
 
-country_table <- table(str_to_title(df$Country))
-country_table
+#country_table <- table(str_to_title(df$Country))
+#country_table
 
-table(df$Region)
+#table(str_to_upper(df$Region))
+
+
