ca2ed5d264229a9bd2e9c51dbea0089de8b9a58c
Fernando Barbeiro <fernandoabcampos@gmail.com>
2018-04-03 18:57:17 +0200

    the swap can be done easily in line 2 - the arrange method of plyr was messing the whole df

--- exercise7swapIfLCIgreaterUCI.R
+++ exercise7swapIfLCIgreaterUCI.R
@@ -1,24 +1,9 @@
-df2 <- read.csv("~/Desktop/Master - Data Science/UOC/Estadística Avanzada/PECs/1-20180403/2016_raw.csv")
-dfAux2 <- df2[df2$Lower.Confidence.Interval>df2$Upper.Confidence.Interval, ]
-df2[df2$Lower.Confidence.Interval > df2$Upper.Confidence.Interval, c("Lower.Confidence.Interval", "Upper.Confidence.Interval")] <- df2[df2$Lower.Confidence.Interval > df2$Upper.Confidence.Interval, c("Upper.Confidence.Interval", "Lower.Confidence.Interval")]
-
-#dfAux <- df[df$LCI>df$UCI, ]
-#dfAux
-#a <- dfAux$LCI
-#b <- dfAux$UCI
-#a = (b - a) + (b = a);
-#dfAux2 <- dfAux
-#dfAux2$LCI <- a
-#dfAux2$UCI <- b
-#dfAux2
-
 
 df[df$LCI > df$UCI, c("LCI", "UCI")] <- df[df$LCI > df$UCI, c("UCI", "LCI")]
 
-# Creating index HR based of read position 
 head(df)
-library(plyr)
-arrange(df, desc(df$HS))
-df$HR <- row(df)
-
+new_df <- df[order(-df$HS),] 
+new_df$HR <- c(1:nrow(df))
+df <- new_df
+head(df)
 
